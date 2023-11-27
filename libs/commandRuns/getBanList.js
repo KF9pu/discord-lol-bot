4ae212442987e56872ba2514345bd26f12a69d90
hong seong hwa <pushan45hsh@gmail.com>
2023-11-27 11:30:19 +0900

    feat : 밴목록 보기 완료

--- libs/commandRuns/getBanList.js
+++ libs/commandRuns/getBanList.js
@@ -1,5 +1,5 @@
 import { CommandInteraction } from "discord.js";
-import { startConsole, catchConsole } from "../index.js";
+import { startConsole, catchConsole, setCommandLog } from "../index.js";
 import { PrismaClient } from "@prisma/client";
 
 /**
@@ -12,9 +12,23 @@ export default async function getBanList(interaction) {
     const user_id = parseInt(interaction.user.id);
     const clan_id = parseInt(interaction.guildId);
 
-    await prisma.ban.findMany({
-      where: {},
-    });
+    await prisma.ban
+      .findMany({
+        where: {
+          clan_id,
+        },
+        orderBy: {
+          ban_dt: "asc",
+        },
+      })
+      .then(banList =>
+        banList.map(
+          ({ champion_name }, index) => `${index + 1}.${champion_name}`
+        )
+      )
+      .then(banChampNames => banChampNames.join("\n"))
+      .then(banChampNames => interaction.reply(`💚 밴 목록\n${banChampNames}`))
+      .then(() => setCommandLog(prisma, user_id, clan_id, "getBanList"));
   } catch (error) {
     catchConsole("getBanList", interaction, error);
   } finally {
